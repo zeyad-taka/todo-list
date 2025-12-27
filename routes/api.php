01800ccd69106023207a6beb8299834ab5faf667
Zeyad-taka <zeyadibrahim184@gmail.com>
2025-12-27 14:28:06 +0200

    uploud

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\taskcontroller;
 use App\Http\Controllers\Api\CategoryController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/tasks', [taskcontroller::class, 'index']);
-Route::get('/tasks/{id}', [taskcontroller::class, 'show']);
-Route::post('/tasks', [taskcontroller::class, 'store']);
-Route::put('/tasks/{id}', [taskcontroller::class, 'update']);
-Route::delete('/tasks/{id}', [taskcontroller::class, 'destroy']);
-Route::get('categories', [App\Http\Controllers\Api\CategoryController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);
