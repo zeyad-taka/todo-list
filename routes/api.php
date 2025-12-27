c8053b526baecc7262aab94881768ea5251ea5ee
Zeyad-taka <zeyadibrahim184@gmail.com>
2025-12-27 13:06:12 +0200

    uploud

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\taskcontroller;
 
-// [17] مسارات الحسابات
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
-// [16 & 18] مسارات المهام
 Route::get('/tasks', [taskcontroller::class, 'index']);
 Route::get('/tasks/{id}', [taskcontroller::class, 'show']);
 Route::post('/tasks', [taskcontroller::class, 'store']);
@@ -16,5 +13,4 @@ Route::put('/tasks/{id}', [taskcontroller::class, 'update']);
 Route::delete('/tasks/{id}', [taskcontroller::class, 'destroy']);
 use App\Http\Controllers\CategoryController;
 
-// مسارات الكاتجوري الجديدة
 Route::apiResource('categories', CategoryController::class);
